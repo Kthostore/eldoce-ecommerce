7e74cec38d93905f82579f0dae6fc5fc74c9ebe7
Kthostore <kthostore@gmail.com>
2025-12-06 19:35:45 -0300

    update app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <div className="p-6 text-center">
       <h1 className="text-2xl font-bold mb-4">
-        Bienvenido a la Tienda — Arquitectura PRO ✔
+        Bienvenido a la Tienda del hicha
       </h1>
 
       <Link
